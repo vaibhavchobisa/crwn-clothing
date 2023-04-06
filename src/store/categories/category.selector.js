da1e7d32e076449401e7a17603690de1043d1f6b
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-04-06 19:58:41 +0530

    commented out console.log(s)

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,7 @@
 import { createSelector } from "reselect";
 
 const selectCategoryReducer = (state) => {
-    console.log('selector 1 fired');
+    // console.log('selector 1 fired');
     return state.categories;
 };
 
@@ -11,7 +11,7 @@ const selectCategoryReducer = (state) => {
 export const selectCategories = createSelector(
     [selectCategoryReducer],
     (categoriesSlice) => {
-        console.log('selector 2 fired');
+        // console.log('selector 2 fired');
         return categoriesSlice.categories;
     }  
 );
@@ -19,7 +19,7 @@ export const selectCategories = createSelector(
 export const selectCategoriesMap = createSelector(
     [selectCategories],
     (categories) => {
-        console.log('selector 3 fired');
+        // console.log('selector 3 fired');
         return categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
