9ef1f2bca737d8e0f506e5c1deaea1bbcd7961d3
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-04-29 03:09:51 +0530

    replaced my middleware with redux logger & wrote logic for it to not work in prod

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,10 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import logger from "redux-logger";
+import logger from "redux-logger";
 // commenting the above logger and writing our own, to demistify middlewares
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -28,10 +15,25 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// const middleWares = [logger]
-const middleWares = [loggerMiddleware]
+// to remove middleware induced console.log(s) from the production environment:-
+const middleWares = [process.env.NODE_ENV !== 'production' && logger]
+.filter(Boolean);
+// Boolean function should be basically:
+// function Boolean(item) {
+//     if (item) return item;
+// }
 
-const composedEnhancers = compose(applyMiddleware(...middleWares)); 
+
+// Using chrome extension for monitoring state changes in Redux
+const composeEnhancer = 
+(process.env.NODE_ENV !== 'production' &&
+window &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+compose; 
+
+// const middleWares = [loggerMiddleware]
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares)); 
 
 export const store = createStore(
     // rootReducer
